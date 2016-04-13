4e85fc1593039187844a6787c1d7762e97f20d0f
Ryalane <ryalane@hotmail.com>
2016-04-12 22:19:04 -0400

    Might have some problems with the new settings
    Huge update to the script
    Added settings for tabbing and alerting instead of titles

--- MQP.user.js
+++ MQP.user.js
@@ -1,29 +1,44 @@
 // ==UserScript==
 // @name         MidenQuestPlus
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.69
 // @description  Provides the user with some enhancements to MidenQuest
 // @author       Ryalane
 // @updateURL    https://github.com/Ryalane/MidenQuestPlus/raw/master/MQP.user.js
 // @match        http://www.midenquest.com/Game.aspx
-// @resource     MainStylesheet https://github.com/Ryalane/MidenQuestPlus/raw/master/style.css
-// @resource     NavbarFile https://github.com/Ryalane/MidenQuestPlus/raw/master/Navbar.html
+// @resource     MainStylesheet https://raw.githubusercontent.com/Ryalane/MidenQuestPlus/master/style.css
+// @resource     NavbarFile https://raw.githubusercontent.com/Ryalane/MidenQuestPlus/master/Navbar.html
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function() {
     'use strict';
     GM_addStyle(GM_getResourceText("MainStylesheet"));
 
+/* Handle Settings */
+// Settings Used
+// Workload_useAlerts = true/false
+// Chat_AllowTabCycling = true/false
+
 /* Change the look of the page slightly */
 
+    GM_xmlhttpRequest({
+  method: "GET",
+  url: "https://raw.githubusercontent.com/Ryalane/MidenQuestPlus/master/Navbar.html",
+  onload: function(response) {
+    $("body").prepend(response.responseText);
+  }
+});
+
     // Create the top navbar
-    var MainBar = $("<div>", {id: "Custom_MainBar"});
-    $("body").prepend(MainBar);
-    $(MainBar).prepend('<h1>Stuff will go here eventually</h1>');
+    //var MainBar = $("<div>", {id: "Custom_MainBar"});
+    //console.log(GM_getResourceText(NavbarFile));
+    //$("body").prepend(GM_getResourceText(NavbarFile));
+    //$(MainBar).prepend('<h1>Stuff will go here eventually</h1>');
 
     // Match the navbar size
     $('#MainPanel').css('width', '1002px');
@@ -31,12 +46,32 @@
     $('.prgActionOverlay').css('margin-top', '-19px');
 
 /* Modify the title of the page */
+    var useAlerts = 'false';
+    var doneAlerting = false;
     var Production = '';
     var ProductionTitle = '';
     var isWorking = false;
 
     var MaxWorkload = 0;
     var CurWorkLoad = 0;
+
+    var CheckWorkloadSettings = function() {
+      if (GM_getValue('Workload_useAlerts')) {
+        if (GM_getValue('Workload_useAlerts') === true) {
+          $('input[name="WorkloadAlert"]').prop('checked', true);
+        } else {
+          // Should be default, but let's do it anyways
+          $('input[name="WorkloadAlert"]').prop('checked', false);
+        }
+      }
+        useAlerts = $('input[name="WorkloadAlert"]').is(':checked');
+
+        // Save every check
+        if (GM_getValue('Workload_useAlerts') !== useAlerts) {
+          GM_setValue('Workload_useAlerts', useAlerts);
+        }
+    };
+
     var CheckWork = function() {
       Production = $('.prgActionOverlay').text();
       if (Production[0] === 'S') {
@@ -63,6 +98,9 @@
         var tempWorkload = Production.split(' ')[1].split('/');
         CurWorkLoad = tempWorkload[0];
         MaxWorkload = tempWorkload[1];
+          if (doneAlerting) {
+              doneAlerting = false;
+          }
       }
     };
 
@@ -74,7 +112,13 @@
         title = ProductionTitle + ' ' + CurWorkLoad + ' / ' + MaxWorkload;
       } else {
         if (ProductionTitle) {
-          title = ProductionTitle + ' has stopped';
+            if (useAlerts && !doneAlerting) {
+
+                alert(ProductionTitle + ' has stopped');
+                doneAlerting = true;
+            } else {
+                title = ProductionTitle + ' has stopped';
+            }
         } else {
           title = 'MidenQuest Online';
         }
@@ -86,6 +130,7 @@
     // Check what the title should be and then update it
 
     var titlething = setInterval(function () {
+    CheckWorkloadSettings();
     CheckWork();
     UpdateTitle();
     }, 1000);
@@ -94,6 +139,27 @@
     var TabContainer = $('.Tabs>ul');
     var tabAmount = $('.Tabs>ul').children().size();
     var selectedTab = 1;
+    var allowTabCycling = true;
+
+    // Put this in an interval eventually and set it to a function instead
+    var ChatSettings = setInterval(function () {
+      // If the value exists
+      if (GM_getValue('Chat_AllowTabCycling')) {
+        // If its set to true, then check the option
+        if (GM_getValue('Chat_AllowTabCycling') === true) {
+          $('input[name="TabCycling"]').prop('checked', true);
+        } else {
+          // Should be default, but let's do it anyways
+          $('input[name="TabCycling"]').prop('checked', false);
+        }
+      }
+        allowTabCycling = $('input[name="TabCycling"]').is(':checked');
+
+        // Save every check
+        if (GM_getValue('Chat_AllowTabCycling') !== allowTabCycling) {
+          GM_setValue('Chat_AllowTabCycling', allowTabCycling);
+        }
+    }, 1000);
     // Create a couple helper functions
     var NextTab = function() {
         // Set what the next tab is
@@ -116,8 +182,11 @@
         var keycode = (e.which) ? e.which : e.keyCode;
         if (keycode == 9)
         {
+          // TODO: Shift + tab for PreviousTab()
+          if (allowTabCycling) {
             NextTab();
             e.preventDefault();
+          }
         }
     });
 })();
