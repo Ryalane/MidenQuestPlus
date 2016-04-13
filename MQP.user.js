ec4738ab676c46b892c9832f1eff6c00ed271b94
Ryalane <ryalane@hotmail.com>
2016-04-12 20:30:14 -0400

    Updated the Production Title Changer
    Modularized it
    Split the string up so I can keep track of the workload
    Works for all resources now

--- MQP.user.js
+++ MQP.user.js
@@ -15,34 +15,84 @@
 
 (function() {
     'use strict';
-    /*jshint multistr: true */
-    // Make the Top navbar
-
     GM_addStyle(GM_getResourceText("MainStylesheet"));
 
+/* Change the look of the page slightly */
+
+    // Create the top navbar
     var MainBar = $("<div>", {id: "Custom_MainBar"});
-
-    $('#MainPanel').css('width', '1002px');
-
     $("body").prepend(MainBar);
-
     $(MainBar).prepend('<h1>Stuff will go here eventually</h1>');
 
-    // Setup the Title bar
-var titlething = setInterval(function () {
-var Selling = $('.prgActionOverlay').text();
+    // Match the navbar size
+    $('#MainPanel').css('width', '1002px');
+    // Center the text a bit better
+    $('.prgActionOverlay').css('margin-top', '-19px');
 
-if (Selling[0] === 'S') {
-document.title = Selling;
-} else {
-document.title = 'Stopped Production';
-}
-}, 1000);
+/* Modify the title of the page */
+    var Production = '';
+    var ProductionTitle = '';
+    var isWorking = false;
 
-    // Setup tab for the chat
-var TabContainer = $('.Tabs>ul');
-var tabAmount = $('.Tabs>ul').children().size();
-var selectedTab = 1;
+    var MaxWorkload
+    var CurWorkLoad
+    var CheckWork = function() {
+      Production = $('.prgActionOverlay').text();
+      if (Production[0] === 'S') {
+        isWorking = true;
+        ProductionTitle = 'Selling';
+      } else if (Production[0] === 'M') {
+        isWorking = true;
+        ProductionTitle = 'Mining';
+      } else if (Production[0] === 'G') {
+        isWorking = true;
+        ProductionTitle = 'Gathering';
+      } else if (Production[0] === 'L') {
+        isWorking = true;
+        ProductionTitle = 'Cutting';
+      } else if (Production[0] === 'F') {
+        isWorking = true;
+        ProductionTitle = 'Fishing';
+      } else {
+        isWorking = false;
+      }
+
+      // After the check, update the current/max workload
+      if (isWorking) {
+        tempWorkload = Production.split(' ')[1].split('/');
+        CurWorkLoad = tempWorkload[0];
+        MaxWorkload = tempWorkload[1];
+      }
+    }
+
+    // Little helper method for setting the title
+    var UpdateTitle = function() {
+      var title = '';
+
+      if (isWorking) {
+        title = ProductionTitle + ' ' + CurWorkLoad + ' / ' + MaxWorkload;
+      } else {
+        if (ProductionTitle) {
+          title = ProductionTitle + ' has stopped';
+        } else {
+          title = 'MidenQuest Online';
+        }
+      }
+
+      document.title = title;
+    }
+
+    // Check what the title should be and then update it
+
+    var titlething = setInterval(function () {
+    CheckWork();
+    UpdateTitle();
+    }, 1000);
+
+/* Handle chat tabs */
+    var TabContainer = $('.Tabs>ul');
+    var tabAmount = $('.Tabs>ul').children().size();
+    var selectedTab = 1;
     // Create a couple helper functions
     var NextTab = function() {
         // Set what the next tab is
@@ -56,17 +106,17 @@ var selectedTab = 1;
         setTimeout(function () { ChangeChatChannel(selectedTab); }, 100);
     };
 
-// Set the tab to #1 just in case
-setTimeout(function () { ChangeChatChannel(1); }, 1000);
+    // Set the tab to #1 just in case
+    setTimeout(function () { ChangeChatChannel(1); }, 1000);
 
 
-// Setup the key events
-$( document ).keydown(function(e) {
-    var keycode = (e.which) ? e.which : e.keyCode;
-    if (keycode == 9)
-    {
-        NextTab();
-        e.preventDefault();
-    }
-});
+    // Setup the key events
+    $( document ).keydown(function(e) {
+        var keycode = (e.which) ? e.which : e.keyCode;
+        if (keycode == 9)
+        {
+            NextTab();
+            e.preventDefault();
+        }
+    });
 })();
