2864233fb5b6f8cba878a035a0b9d36b48b9ebfb
Ryalane <ryalane@hotmail.com>
2016-04-14 03:27:39 -0400

    v0.97
    Added html sanitizing

--- MQP.beta.user.js
+++ MQP.beta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MidenQuestPlus
 // @namespace    MidenQuestPlus_tampermonkey
-// @version      0.96
+// @version      0.97
 // @description  Provides the user with some enhancements to MidenQuest
 // @author       Ryalane
 // @updateURL    https://github.com/Ryalane/MidenQuestPlus/raw/master/MQP.user.js
@@ -222,6 +222,37 @@ Settings.addInput(2, "mentionBackground", "Mention Background Colour", "rgba(255
 
 /* CHAT METHODS AND LOGIC GO HERE */
 
+var protos = document.body.constructor === window.HTMLBodyElement;
+var validHTMLTags  =/^(?:a|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|bgsound|big|blink|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|data|datalist|dd|del|details|dfn|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hgroup|hr|html|i|iframe|img|input|ins|isindex|kbd|keygen|label|legend|li|link|listing|main|map|mark|marquee|menu|menuitem|meta|meter|nav|nobr|noframes|noscript|object|ol|optgroup|option|output|p|param|plaintext|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|spacer|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr|xmp)$/i;
+
+function sanitize(txt) {
+    var // This regex normalises anything between quotes
+        normaliseQuotes = /=(["'])(?=[^\1]*[<>])[^\1]*\1/g,
+        normaliseFn = function ($0, q, sym) {
+            return $0.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        },
+        replaceInvalid = function ($0, tag, off, txt) {
+            var
+                // Is it a valid tag?
+                invalidTag = protos &&
+                    document.createElement(tag) instanceof HTMLUnknownElement
+                    || !validHTMLTags.test(tag),
+
+                // Is the tag complete?
+                isComplete = txt.slice(off+1).search(/^[^<]+>/) > -1;
+
+            return invalidTag || !isComplete ? '&lt;' + tag : $0;
+        };
+
+    txt = txt.replace(normaliseQuotes, normaliseFn)
+             .replace(/<(\w+)/g, replaceInvalid);
+
+    var tmp = document.createElement("DIV");
+    tmp.innerHTML = txt;
+
+    return "textContent" in tmp ? tmp.textContent : tmp.innerHTML;
+}
+
 var ParseChat = function(MessageContainer) {
   var Message;
   var Message_timeStamp;
@@ -282,11 +313,11 @@ var ParseChat = function(MessageContainer) {
             // This is the stuff we don't want
           } else if (i > 2) {
             // This is what the user sent that got cut out; so add a :
-            Message_Text += ":" + Message_Text_Array[i];
+            Message_Text += ":" + sanitize(Message_Text_Array[i]);
             // Can use += since it'll always be after the normal message
           } else {
             // The first part of the message
-            Message_Text = Message_Text_Array[i].substring(1);
+            Message_Text = sanitize(Message_Text_Array[i].substring(1));
           }
         }
       }
