7af17ae5559a3e79f76b89c463e6818821866f54
Ryalane <ryalane@hotmail.com>
2016-04-15 00:36:45 -0400

    Now working
    Works when you switch tabs and normal messages
    Intercepts the normal handler for this, but lets it do everything else

--- ServerTest.js
+++ ServerTest.js
@@ -1,19 +1,50 @@
+var PageSettings = PageSettings || {};
+
+PageSettings.SetStyle = function (StyleRules) {
+  $( "<style>" + StyleRules + "</style>").appendTo( "head" );
+}
+
+/*
+var Timestamp = '<span class="chat-timestamp">' + Message.Timestamp + '</span>';
+var Title = '<span class="chat-title" onclick="' + Message.UserPage + '" style="color: ' + Message.Title.Color + '">' + Message.Title.Text + ' </span>';
+var Name = '<span class="chat-name" onclick="' + Message.UserPage + '">' + Message.Username + ': </span>';
+var MessageText = '<span class="chat-message">' + Message.Text + '</span>';
+$('#ChatLog').prepend('<div class="chat-shout" id="' + UniqueID + '"></div>');
+*/
+PageSettings.SetupPage = function () {
+  // Setup the chat
+  this.SetStyle(".chat-shout { font-size: 16px; padding: 2px; } .chat-shout:nth-child(even) { background: #FFF; } .chat-shout:nth-child(odd) { background: #e6e6e6; } .chat-title { font-weight: bold; } .chat-name { font-weight: bold; }");
+}
+
+PageSettings.SetupPage();
+
 var ServerMessages = ServerMessages || {};
 
 ServerMessages.Options = {
-          CD:               "SETCD",
-          ChatNotification: "CHATNOTIF",
-          GetName:          "SETNAME",
-          LoadLog:          "LOGSTART",
-          Log:              "NLOG",
-          Location:         "SETLOC",
-          Message:          "CHATNEW",
-          ChatStarted:      "CHATSTART",
-          Channel:          "CHANNEL",
-          Timer:            "TIMER",
-          LoadPage:         "LOADPAGE",
-          TSData:           "TSLVL",
-          Ore:              "SETORE"};
+    CD:               "SETCD",
+    ChatNotification: "CHATNOTIF",
+    GetName:          "SETNAME",
+    LoadLog:          "LOGSTART",
+    Log:              "NLOG",
+    Location:         "SETLOC",
+    Message:          "CHATNEW",
+    ChatStarted:      "CHATSTART",
+    Channel:          "CHANNEL",
+    Timer:            "TIMER",
+    LoadPage:         "LOADPAGE",
+    TSData:           "TSLVL",
+    Ore:              "SETORE"
+};
+
+ServerMessages.GenerateUniqueID = function (length = 6) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < length; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
 
 ServerMessages.Message = function(a_Text, a_Username, a_Title, a_Link, a_Timestamp, a_isEmpty = false, a_isNotification = false) {
   this.Text = a_Text;
@@ -44,7 +75,7 @@ ServerMessages.ParseMessage = function(data) {
   var Message_Text_Array;
   var regex_Message_Text = /word-wrap: break-word(.*)<\/span>/i;
   var Message_Text;
-  var regex_Message_Link = /\((.*)\)/i;
+  var regex_Message_Link = /onclick="(.*)event.preventDefault\(\);/i;
   var Message_Link;
   var regex_isNotification = /<span style='font-weight: bold; color: #253ECC;(.*)<br \/>/i;
   var isNotification = false;
@@ -53,7 +84,8 @@ ServerMessages.ParseMessage = function(data) {
   if (Message) {
     var temp = String(Message).match(regex_isNotification);
     if (temp) {
-      temp = new this.Message(Message, "", {Text: "", Color: ""}, "", "");
+      temp = new this.Message(Message, "", {Text: "", Color: ""}, "", "", false, true);
+      this.PostToChatBox(temp);
       isNotification = true;
     }
   }
@@ -65,140 +97,73 @@ ServerMessages.ParseMessage = function(data) {
     tempNameTitle = String(Message).match(regex_Name_and_Title)[1].split(' ');
     Message_Name = tempNameTitle[1];
     Message_Title = tempNameTitle[0].slice(0,-7);
-    Message_Link = "sendRequestContentFill" + String(Message).match(regex_Message_Link)[0] + ";";
+    Message_Link = String(Message).match(regex_Message_Link)[0];
     Message_Title_Color = String(Message).match(regex_Title_Color)[2];
     Message_timeStamp = String(Message).match(regex_Time_Stamp)[3];
 
     temp = new this.Message(Message_Text, Message_Name, {Text: Message_Title, Color: Message_Title_Color}, Message_Link, Message_timeStamp);
-    console.log(temp);
+    this.PostToChatBox(temp)
 
   }
 }
 
 /* LONG PARSE METHOD IS DONE */
 
+ServerMessages.PostToChatBox = function(Message) {
+  // Check if the message is empty or null
+  if (!Message.isEmpty && Message) {
+    // Check if its a notification
+    if (Message.isNotification) {
+      $('#ChatLog').prepend(Message.Text);
+    } else {
+      var UniqueID = this.GenerateUniqueID(10);
+      var Timestamp = '<span class="chat-timestamp">' + Message.Timestamp + '</span>';
+      var Title = '<span class="chat-title" onclick="' + Message.UserPage + '" style="color: ' + Message.Title.Color + '">' + Message.Title.Text + ' </span>';
+      var Name = '<span class="chat-name" onclick="' + Message.UserPage + '">' + Message.Username + ': </span>';
+      var MessageText = '<span class="chat-message">' + Message.Text + '</span>';
+      $('#ChatLog').prepend('<div class="chat-shout" id="' + UniqueID + '"></div>');
+      $('#' + UniqueID).append(Timestamp);
+      $('#' + UniqueID).append(Title);
+      $('#' + UniqueID).append(Name);
+      $('#' + UniqueID).append(MessageText);
+    }
+  }
+
+}
+
 ServerMessages.getOption = function(data) {
   var Command = data.split('|')[0];
 
   var OptionSize = Object.keys(this.Options).length;
+  tempData = data;
   for (var i = 0; i < OptionSize; i++) {
 
     var key = Object.keys(this.Options)[i];
     var value = this.Options[key];
-    console.log("Checking key: " + key + ", value: " + value);
     if (value === Command) {
       // If the value is a known command, then get the parser up
       //TODO: Intercept ONLY messages
-      switch (key) {
-        case "Message":
-          this.ParseMessage();
-        break;
-        case "ChatStarted":
-          //this.ParseMessage();
-        break;
+      if (key === "Message") {
+        this.ParseMessage(data);
+      } else if (key === "ChatStarted") {
+        // Loop through all the messages and parse them
+        $('#ChatLog').empty();
+        var tempregex = /<div id='chatShout1'>(.*?)<\/div>/g;
+        var matches = data.match(tempregex);
+        for (var j = matches.length - 1; j >= 0; j--) {
+          tempMatch = "CHATSTART|" + matches[j];
+          this.ParseMessage(tempMatch);
+        }
+
+      } else {
+        ServerReceptionHandler(data);
       }
     }
   }
 }
 
-var ServerMessage = {
-  Data: "",
-  Options: {CD:               "SETCD",
-            ChatNotification: "CHATNOTIF",
-            LoadLog:          "LOGSTART",
-            Log:              "NLOG",
-            Location:         "SETLOC",
-            Message:          "CHATNEW",
-            ChatStarted:      "CHATSTART",
-            Channel:          "CHANNEL",
-            Timer:            "TIMER",
-            LoadPage:         "LOADPAGE",
-            TSData:           "TSLVL",
-            Ore:              "SETORE",},
-
-  getOption: function(data) {
-    this.Data = data;
-    var Command = this.Data.split('|')[0];
-
-    var OptionSize = Object.keys(this.Options).length;
-    for (var i = 0; i < OptionSize; i++) {
-      var key = Object.keys(this.Options)[i];
-      var value = this.Options[key];
-      if (value === Command) {
-        // If the value is a known command, then get the parser up
-        //TODO: Intercept ONLY messages
-        switch (key) {
-          case "CD":
-            this.ParseCD();
-          break;
-          case "Message":
-            this.ParseMessage();
-          break;
-          case "ChatStarted":
-            //this.ParseMessage();
-          break;
-        }
-      }
-    }
-  },
-
-  ParseCD: function() {
-    var splitData = this.Data.split('|');
-    var TimeRemaining = splitData[1];
-    var TotalTime = splitData[2];
-    var WorkloadString = splitData[3];
-
-    console.log(WorkloadString);
-  },
-  ParseMessage: function() {
-    var splitData = this.Data.split('|');
-    // Not checking for any other pipes because it doesnt show anyways
-    splitData.shift();
-    // Setup variables
-    var Message = splitData;
-    var regex_Time_Stamp = /( )(.{16})(.{7})/i;
-    var Message_timeStamp;
-    var regex_Name_and_Title = /style="font-size:smaller;">(.*)<\/a>/i;
-    var Message_Name;
-    var Message_Title;
-    var regex_Title_Color = /(style="color:)(.{7})/i;
-    var Message_Title_Color;
-    var Message_Text_Array;
-    var regex_Message_Text = /word-wrap: break-word(.*)<\/span>/i;
-    var Message_Text;
-    var regex_Message_Link = /\((.*)\)/i;
-    var Message_Link;
-    var m;
-
-    var regex_isNotification = /<span style='font-weight: bold; color: #253ECC;(.*)<br \/>/i;
-    // Check if it's a notification first
-    if (Message) {
-      var temp = String(Message).match(regex_isNotification);
-      console.log(temp);
-    }
-    // Check just to make sure the message isn't empty
-    if (Message) {
-      // Get the title colour
-      Message_Text = String(Message).match(regex_Message_Text)[1].substring(3);
-      tempNameTitle = String(Message).match(regex_Name_and_Title)[1].split(' ');
-      Message_Name = tempNameTitle[1];
-      Message_Title = tempNameTitle[0].slice(0,-7);
-      Message_Link = "sendRequestContentFill" + String(Message).match(regex_Message_Link)[0] + ";";
-      Message_Title_Color = String(Message).match(regex_Title_Color)[2];
-      Message_timeStamp = String(Message).match(regex_Time_Stamp)[3];
-
-      //temp = new ServerMessages.Message(Message_Text, Message_Name, {Text: Message_Title, Color: Message_Title_Color}, Message_Link, Message_timeStamp);
-      //console.log(temp);
-
-    }
-
-    // End ParseMessage
-  }
-};
-
 function onmsg(evt) {
-ServerReceptionHandler(evt.data);
-ServerMessage.getOption(evt.data);
+ServerMessages.getOption(evt.data);
 };
 // Start watching incoming messages
 ws.onmessage=onmsg;
